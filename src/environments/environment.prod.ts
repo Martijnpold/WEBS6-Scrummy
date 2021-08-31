88b319e9f8967e49a3a65cddee19f46e21296606
MartijnPold <martijntje7@gmail.com>
2021-08-31 02:22:15 +0200

    Revert

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: true,
+    firebase: {
+      apiKey: "AIzaSyBVmmrFaYOO7Fs_NTFn8ps5GtqanXwpRyg",
+      authDomain: "scrummy-fa053.firebaseapp.com",
+      databaseURL: "https://scrummy-fa053.firebaseio.com",
+      projectId: "scrummy-fa053",
+      storageBucket: "scrummy-fa053.appspot.com",
+      messagingSenderId: "522207768570",
+      appId: "1:522207768570:web:e384898e52e5c333027e43",
+      measurementId: "G-W1NV7GHZD7"
+    }
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyBVmmrFaYOO7Fs_NTFn8ps5GtqanXwpRyg",
+      authDomain: "scrummy-fa053.firebaseapp.com",
+      databaseURL: "https://scrummy-fa053.firebaseio.com",
+      projectId: "scrummy-fa053",
+      storageBucket: "scrummy-fa053.appspot.com",
+      messagingSenderId: "522207768570",
+      appId: "1:522207768570:web:e384898e52e5c333027e43",
+      measurementId: "G-W1NV7GHZD7"
+    }
+  };
+  
+  /*
+   * For easier debugging in development mode, you can import the following file
+   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+   *
+   * This import should be commented out in production mode because it will have a negative impact
+   * on performance if an error is thrown.
+   */
+  // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  
